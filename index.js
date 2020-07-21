a0a044a4923d796b19668da16e1ff7ddf61bee94
2imad <imadyoussoufi@gmail.com>
2020-07-21 21:23:31 +0200

    fix babel

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 import React, { useReducer } from "react";
-
 export default (reducer, actions, initialState) => {
-  const Context = React.createContext();
+  const Context = /*#__PURE__*/ React.createContext();
 
   const Provider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
     const boundActions = {};
+
     for (let key in actions) {
       boundActions[key] = actions[key](dispatch);
     }
 
-    return (
-      <Context.Provider value={{ state, ...boundActions }}>
-        {children}
-      </Context.Provider>
+    return /*#__PURE__*/ React.createElement(
+      Context.Provider,
+      {
+        value: {
+          state,
+          ...boundActions,
+        },
+      },
+      children
     );
   };
-  return { Context, Provider };
+
+  return {
+    Context,
+    Provider,
+  };
 };
